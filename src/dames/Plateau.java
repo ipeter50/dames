4e09264770fdbfd116443b654116d2bc189a49fa
theJpay <jules.paris@wanadoo.fr>
2017-11-30 11:35:11 +0100

    added method affichePlateau

--- src/dames/Plateau.java
+++ src/dames/Plateau.java
@@ -15,24 +15,58 @@ public class Plateau {
 
     ArrayList<ArrayList<Pion>> damier;
 
+    public ArrayList<ArrayList<Pion>> getDamier() {
+        return damier;
+    }
+
+    public void setDamier(ArrayList<ArrayList<Pion>> damier) {
+        this.damier = damier;
+    }
+
     public Plateau() {
+        damier = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             damier.add(new ArrayList<>());
             for (int j = 0; j < 10; j++) {
                 damier.get(i).add(null);
             }
         }
-        System.out.println(damier.toString());
         for (int i = 0; i < 20; i++) {
-            int y = i % 5;
-            int x = (2 * i) % y + y % 2;
-            damier.get(x).set(y, new Pion([x, y], "N"));
+            int x = i / 5;
+            int y = (2 * i) % 10 + ((x + 1) % 2);
+            int[] pos = new int[2];
+            pos[0] = x;
+            pos[1] = y;
+            damier.get(x).set(y, new Pion(pos, "N"));
         }
         for (int i = 0; i < 20; i++) {
-            int y = 9 - (i % 5);
-            int x = 9- ((2 * i) % y + y%2);
-            damier.get(x).set(y, new Pion([x, y], "B"));            
+            int x = 9 - (i / 5);
+            int y = 9 - ((2 * i) % 10 + x % 2);
+            int[] pos = new int[2];
+            pos[0] = x;
+            pos[1] = y;
+            damier.get(x).set(y, new Pion(pos, "B"));
         }
-        System.out.println(damier.toString());
+    }
+
+    public void affichePlateau() {
+        int k = damier.size();
+        int l = damier.size();
+        System.out.println(k+" "+l);
+        for (int i = 0; i < k; i++) {
+            String ligne = new String();
+            for (int j = 0; j < k; j++) {
+                if (damier.get(i).get(j) == null) {
+                    ligne+="|   ";
+                } else {
+                    if (damier.get(i).get(j).isEstNoir()) {
+                        ligne+="| N ";
+                    } else {
+                        ligne+="| B ";
+                    }
+                }
+            }
+            System.out.println(ligne+"|\n");
+        }
     }
 }
