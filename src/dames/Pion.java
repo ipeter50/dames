01f586273aad393d2f9c4ae4267defc408a5ac00
pacom <pacom@130.66.204.87>
2017-11-30 12:06:02 +0100

    Fin scéance

--- src/dames/Pion.java
+++ src/dames/Pion.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
  * @author Pierre
  */
 public class Pion {
+
     private int[] coordonnees; //[y;x]
     private boolean estDame;
     private boolean estNoir;
@@ -39,37 +40,70 @@ public class Pion {
     public void setEstNoir(boolean estNoir) {
         this.estNoir = estNoir;
     }
-    
+
     public Pion(int[] coordonnees, String couleur) {
         this.coordonnees = coordonnees;
-        this.estDame=false;
-        if (couleur.equals("N")){
-            this.estNoir=true;
-        }
-        else{
-            this.estNoir=false;
+        this.estDame = false;
+        if (couleur.equals("N")) {
+            this.estNoir = true;
+        } else {
+            this.estNoir = false;
         }
     }
-    
-    public boolean deplacable(Plateau p){
-        boolean res=true;
-        if(this.estNoir){
-            if(!this.estDame){
-                ArrayList<ArrayList<Pion>> damier= p.getDamier();
-                if(damier.get(this.coordonnees[0]+1).get(coordonnees[1]+1)!= null){
-                    res=false;
-                }
-                else if (damier.get(this.coordonnees[0]+1).get(coordonnees[1]-1)!= null){
-                    res=false;
+
+    public boolean deplacable(Plateau p) {
+        boolean res = true;
+        if (this.estNoir) {
+            if (!this.estDame) {
+                ArrayList<ArrayList<Pion>> damier = p.getDamier();
+                if (damier.get(this.coordonnees[0] + 1).get(coordonnees[1] + 1) != null) {
+                    res = false;
+                } else if (damier.get(this.coordonnees[0] + 1).get(coordonnees[1] - 1) != null) {
+                    res = false;
                 }
             }
         }
-        
-        
-        
+
         return res;
-        
+
     }
-    
-    
+
+    public boolean peutManger(Plateau p) {
+        boolean res = false;
+        for (int k = 0; k < 4; k++) {
+            int x, y;
+            switch (k) {
+                case 0:
+                    y = this.coordonnees[0] - 1;
+                    x = this.coordonnees[1] + 1;
+                    break;
+                case 1:
+                    y = this.coordonnees[0] + 1;
+                    x = this.coordonnees[1] + 1;
+                    break;
+                case 2:
+                    y = this.coordonnees[0] + 1;
+                    x = this.coordonnees[1] - 1;
+                    break;
+                case 3:
+                    y = this.coordonnees[0] - 1;
+                    x = this.coordonnees[1] - 1;
+                    break;     
+                default:
+                    y = -1;
+                    x = -1;
+                    break;
+            }
+            if ((x >= 0) && (x <10) && (y >= 0) && (y < 10)){
+                if (p.damier.get(y).get(x) != null){
+                    res = p.damier.get(y).get(x).estNoir != this.estNoir;
+                }
+            }
+        }
+        int y = this.coordonnees[0] - 1;
+        int x = this.coordonnees[1] + 1;
+
+        return res;
+    }
+
 }
